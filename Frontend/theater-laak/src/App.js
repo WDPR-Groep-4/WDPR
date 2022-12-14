ae2db3291987d0015e4e8f93dcb0f986a1acc130
Jonathan Meij <jonathanvanmeij@gmail.com>
2022-12-14 22:33:33 +0100

    routes toegevoegd

--- Frontend/theater-laak/src/App.js
+++ Frontend/theater-laak/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/pages/home/HomePage";
+
 function App() {
-    return <div className="App">=</div>;
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<HomePage />} exact />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- Frontend/theater-laak/src/index.js
+++ Frontend/theater-laak/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "@fontsource/outfit";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
