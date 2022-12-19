8d588e4ee09a505a97a6a77a9dc4279000af59be
Jonathan Meij <jonathanvanmeij@gmail.com>
2022-12-19 19:25:07 +0100

    Voorstelling pagina toegevoegd

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -2,6 +2,7 @@ import { FetchData } from "./components/FetchData";
 import HomePage from "./components/pages/home/HomePage";
 import Login from "./components/pages/authenticatie/LoginPage";
 import Registreer from "./components/pages/authenticatie/RegistreerPage";
+import VoorstellingPage from "./components/pages/voorstelling/VoorstellingPage";
 
 const AppRoutes = [
     {
@@ -20,9 +21,14 @@ const AppRoutes = [
         path: "/registreer",
         element: <Registreer />,
     },
+
     {
         path: "*",
         element: <HomePage />,
+    },
+    {
+        path: "/voorstelling/:id",
+        element: <VoorstellingPage />,
     },
 ];
 
