fe3f5a056af0e6ad2dbbdd9355cb0fa58cceb995
Jonathan Meij <jonathanvanmeij@gmail.com>
2022-12-19 14:39:37 +0100

    replaced box to card

--- ClientApp/src/components/pages/winkelwagen/Winkelwagen.js
+++ ClientApp/src/components/pages/winkelwagen/Winkelwagen.js
@@ -1,42 +1,59 @@
 import { Box, Container } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Card, Divider, Stack, Typography } from "@mui/material";
 import Product from "./Product";
 
 //https://reactjs.org/docs/lists-and-keys.html
 
 export default function Winkelwagen(props) {
     return (
-        <Container maxWidth="xl">
-            <Box>
-                <Typography variant="h4" component="h1" sx={{ my: 2 }}>
-                    Winkelwagen
-                </Typography>
-            </Box>
-            <Box
-                sx={{
-                    display: "flex",
-                    gap: 2,
-                    flexDirection: { sm: "column", md: "row" },
-                }}
-            >
+        <Box sx={{ backgroundColor: "#f5f5f5", minHeight: "100vh" }}>
+            <Container maxWidth="xl">
+                <Box>
+                    <Typography variant="h4" component="h1" sx={{ py: 2 }}>
+                        Winkelwagen
+                    </Typography>
+                </Box>
                 <Box
                     sx={{
-                        border: 2,
-                        borderColor: "#EBEBEB",
-                        flexGrow: { sm: 1, md: 2 },
+                        display: "flex",
+                        gap: 2,
+                        flexDirection: { sm: "column", md: "row" },
                     }}
                 >
-                    <h3>Producten</h3>
-                </Box>
-                <Box sx={{ border: 2, flexGrow: 1, borderColor: "#EBEBEB" }}>
-                    <span
-                        sx={{ fontSize: 24, fontFamily: "Outfit", fontStyle: "SemiBold" }}
+                    <Card
+                        sx={{
+                            flexGrow: { sm: 1, md: 2 },
+                            p: 2,
+                        }}
+                        variant="outlined"
                     >
-                        Totaal
-                    </span>
-                    <Product />
+                        <h3>Producten</h3>
+                        <Divider sx={{ mb: 1 }} />
+                        <Stack
+                            direction="column"
+                            sx={{
+                                gap: 2,
+                            }}
+                        >
+                            <Product />
+                            <Product />
+                            <Product />
+                        </Stack>
+                    </Card>
+                    <Card sx={{ flexGrow: 1, p: 2 }} variant="outlined">
+                        <span
+                            sx={{
+                                fontSize: 24,
+                                fontFamily: "Outfit",
+                                fontStyle: "SemiBold",
+                            }}
+                        >
+                            Totaal
+                        </span>
+                        <Divider sx={{ mb: 1 }} />
+                    </Card>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        </Box>
     );
 }
