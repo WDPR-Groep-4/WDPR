58108c70c41adbebd8062c7aa70fb6dc1b72dde4
Jonathan Meij <jonathanvanmeij@gmail.com>
2022-12-19 10:24:51 +0100

    grid

---
+++ ClientApp/src/components/pages/winkelwagen/Product.js
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+export default function Product(props) {
+    return (
+        <Box>
+            <Typography>product</Typography>
+        </Box>
+    );
+}
--- ClientApp/src/components/pages/winkelwagen/Winkelwagen.js
+++ ClientApp/src/components/pages/winkelwagen/Winkelwagen.js
@@ -1,40 +1,42 @@
 import { Box, Container } from "@mui/system";
+import { Typography } from "@mui/material";
+import Product from "./Product";
+
+//https://reactjs.org/docs/lists-and-keys.html
 
 export default function Winkelwagen(props) {
-  return (
-    <Container
-      sx={{
-        maxWidth: "xl",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Box>
-        <span sx={{ fontSize: 36, fontFamily: "Outfit", fontStyle: "Medium" }}>
-          Winkelwagen
-        </span>
-      </Box>
-      <Box sx={{ display: "flex" }}>
-        <Box
-          sx={{
-            border: 2,
-            borderColor: "#EBEBEB",
-            m: 2,
-            flexGrow: 1,
-            width: 0.4,
-          }}
-        >
-          <h3>Producten</h3>
-        </Box>
-        <Box sx={{ border: 2, borderColor: "#EBEBEB", m: 2, width: 0.3 }}>
-          <span
-            sx={{ fontSize: 24, fontFamily: "Outfit", fontStyle: "SemiBold" }}
-          >
-            Totaal
-          </span>
-        </Box>
-      </Box>
-    </Container>
-  );
+    return (
+        <Container maxWidth="xl">
+            <Box>
+                <Typography variant="h4" component="h1" sx={{ my: 2 }}>
+                    Winkelwagen
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    gap: 2,
+                    flexDirection: { sm: "column", md: "row" },
+                }}
+            >
+                <Box
+                    sx={{
+                        border: 2,
+                        borderColor: "#EBEBEB",
+                        flexGrow: { sm: 1, md: 2 },
+                    }}
+                >
+                    <h3>Producten</h3>
+                </Box>
+                <Box sx={{ border: 2, flexGrow: 1, borderColor: "#EBEBEB" }}>
+                    <span
+                        sx={{ fontSize: 24, fontFamily: "Outfit", fontStyle: "SemiBold" }}
+                    >
+                        Totaal
+                    </span>
+                    <Product />
+                </Box>
+            </Box>
+        </Container>
+    );
 }
