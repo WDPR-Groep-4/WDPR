d66f8b528d7a5600d22e19fb04eeda041918fa99
Jonathan Meij <jonathanvanmeij@gmail.com>
2022-12-23 19:47:19 +0100

    Update BevestigEmailPage.js

--- ClientApp/src/components/pages/authenticatie/BevestigEmailPage.js
+++ ClientApp/src/components/pages/authenticatie/BevestigEmailPage.js
@@ -1,14 +1,17 @@
-import { Container } from "@mui/system";
+import { Container, Box } from "@mui/system";
 import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useAuthUser } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import { Button } from "@mui/material";
 
 import axios from "axios";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 export default function BevestigEmailPage(props) {
     const [isLoading, setIsLoading] = useState(true);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
 
     const userId = searchParams.get("userId");
     const token = searchParams.get("token");
@@ -32,15 +35,45 @@ export default function BevestigEmailPage(props) {
 
     return (
         <Container maxWidth="xl">
-            {isLoading ? (
-                <Typography variant="body1" component={"p"}>
-                    Gegevens laden...
-                </Typography>
-            ) : (
-                <Typography variant="body1" component={"p"}>
-                    email bevestigd!
-                </Typography>
-            )}
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "90vh",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: 2,
+                }}
+            >
+                <MarkEmailReadIcon sx={{ fontSize: 100, margin: -1.5 }} />
+                <Box
+                    sx={{
+                        maxWidth: 450,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        textAlign: "center",
+                    }}
+                >
+                    {isLoading ? (
+                        <Typography variant="body1" component={"p"}>
+                            Gegevens laden...
+                        </Typography>
+                    ) : (
+                        <Typography variant="h5" component={"h1"}>
+                            email bevestigd
+                        </Typography>
+                    )}
+                    <Typography variant="body1" component={"p"}>
+                        Uw email is bevestigd. U kunt nu uw account gebruiken.
+                    </Typography>
+                </Box>
+                <Button onClick={() => navigate("/")} variant="contained">
+                    naar Home pagina
+                </Button>
+            </Box>
         </Container>
     );
 }
