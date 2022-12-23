c24d208f48e47c15d0b410af8564e86ff48459ed
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2022-12-23 13:38:02 +0100

    api test

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -7,7 +7,9 @@ const target = env.ASPNETCORE_HTTPS_PORT
     ? env.ASPNETCORE_URLS.split(";")[0]
     : "http://localhost:25568";
 
-const context = ["/weatherforecast", "/api/auth/login", "/api/auth/registreer", "/api"];
+// const context = ["/weatherforecast", "/api/auth/login", "/api/auth/registreer", "/api"];
+const context = ["/api"];
+
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
