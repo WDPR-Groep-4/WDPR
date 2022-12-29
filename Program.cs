69447497670f7835633c587e3e37d0cefaf77253
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2022-12-29 13:51:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -62,6 +62,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html"); ;
+app.MapFallbackToFile("index.html");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DatabaseContext>();
+    context.Database.EnsureCreated();
+}
 
 app.Run();
