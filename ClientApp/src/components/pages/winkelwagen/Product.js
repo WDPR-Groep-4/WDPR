0c37871735d3a01c712749c6cba4e87122a0cc26
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2023-01-11 16:38:08 +0100

    Verwijder Button

--- ClientApp/src/components/pages/winkelwagen/Product.js
+++ ClientApp/src/components/pages/winkelwagen/Product.js
@@ -5,87 +5,125 @@ import DeleteButton from "./DeleteButton";
 import { useWinkelWagen } from "../../../services/WinkelwagenContext";
 
 export default function Product(props) {
-    const { item, setTotaal } = props;
-    const voorstelling = item.voorstelling;
-    const rang = item.rang;
-    const hoeveelheid = item.hoeveelheid;
-    const prijs = voorstelling.prijzenPerRang.find((item) => item.rang === rang);
-    const { removeFromWinkelwagen } = useWinkelWagen();
+  const { item, setTotaal } = props;
+  const voorstelling = item.voorstelling;
+  const rang = item.rang;
+  const hoeveelheid = item.hoeveelheid;
+  const prijs = voorstelling.prijzenPerRang.find((item) => item.rang === rang);
+  const { removeFromWinkelwagen } = useWinkelWagen();
 
-    function onDelete() {
-        removeFromWinkelwagen(item.id);
-        setTotaal((prev) => prev - prijs.prijs * hoeveelheid);
-    }
+  function onDelete() {
+    removeFromWinkelwagen(item.id);
+    setTotaal((prev) => prev - prijs.prijs * hoeveelheid);
+  }
 
-    return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: { md: "row", sm: "row", xs: "column" },
-                backgroundColor: "#FFFFFF",
-                border: "1px solid #0000001f",
-                width: { xs: "100%", sm: "100%", md: "100%" },
-            }}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { md: "row", sm: "row", xs: "column" },
+        backgroundColor: "#FFFFFF",
+        border: "1px solid #0000001f",
+        width: { xs: "100%", sm: "100%", md: "100%" },
+      }}
+    >
+      <img
+        src={voorstelling.afbeelding}
+        alt="product"
+        style={{
+          maxWidth: "12rem",
+          maxHeight: "12rem",
+          height: "25vw",
+          width: "25vw",
+          borderRight: "1px solid #0000001f",
+        }}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 1,
+          ml: 3,
+          flexGrow: 2,
+        }}
+      >
+        <Typography
+          variant="h6"
+          component="h3"
+          sx={{ fontWeight: 300, fontSize: 15, paddingTop: 2 }}
         >
-            <img
-                src={voorstelling.afbeelding}
-                alt="product"
-                style={{
-                    maxWidth: "12rem",
-                    maxHeight: "12rem",
-                    height: "25vw",
-                    width: "25vw",
-                    borderRight: "1px solid #0000001f",
-                }}
-            />
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 1, ml: 3 }}>
-                <Typography
-                    variant="h6"
-                    component="h3"
-                    sx={{ fontWeight: 300, fontSize: 15, paddingTop: 2 }}
-                >
-                    Musical
-                </Typography>
-                <Typography
-                    variant="h4"
-                    component="h2"
-                    sx={{ fontWeight: 500, fontSize: 24, marginTop: -1.5 }}
-                >
-                    {voorstelling.titel}
-                </Typography>
-            </Box>
-            <Box
-                sx={{
-                    alignItems: "center",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    flexGrow: 1,
-                    gap: 2,
-                    pr: 3,
-                }}
-            >
-                <Typography
-                    variant="h6"
-                    component="h3"
-                    sx={{
-                        fontWeight: 400,
-                        fontSize: 14,
-                    }}
-                >
-                    Aantal: {hoeveelheid} x
-                </Typography>
-                <Typography
-                    variant="h6"
-                    component="h3"
-                    sx={{
-                        fontWeight: 500,
-                        fontSize: 20,
-                    }}
-                >
-                    € {prijs.prijs * hoeveelheid}
-                </Typography>
-            </Box>
-            <DeleteButton onDelete={onDelete}></DeleteButton>
+          Musical
+        </Typography>
+        <Typography
+          variant="h4"
+          component="h2"
+          sx={{ fontWeight: 500, fontSize: 24, marginTop: -1.5 }}
+        >
+          {voorstelling.titel}
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column", pr: 3 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            flexGrow: 0.1,
+            justifyContent: "flex-end",
+            gap: 1,
+            mt: 1,
+          }}
+        >
+          <Button
+            variant="text"
+            endIcon={
+              <img
+                src="./delete.png"
+                style={{ maxWidth: "16px", maxHeight: "16px" }}
+              />
+            }
+            onClick={onDelete}
+            sx={{
+              fontWeight: 400,
+              fontSize: 10,
+              color: "#727272",
+            }}
+          >
+            Verwijderen
+          </Button>
         </Box>
-    );
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            flexGrow: 0.5,
+            gap: 1,
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="h3"
+            sx={{
+              fontWeight: 400,
+              fontSize: 14,
+            }}
+          >
+            Aantal: {hoeveelheid} x
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h3"
+            sx={{
+              fontWeight: 500,
+              fontSize: 20,
+            }}
+          >
+            € {prijs.prijs * hoeveelheid}
+          </Typography>
+        </Box>
+        <DeleteButton onDelete={onDelete}></DeleteButton>
+      </Box>
+    </Box>
+  );
 }
