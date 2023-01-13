2a410a9acbd8652272a3d0534d8c25ad5bc306ba
jonioak <101118421+jonioak@users.noreply.github.com>
2023-01-13 03:37:18 +0100

    Agenda pagina aangepast
    Voor mobiele gebruikers

--- ClientApp/src/components/pages/agenda/AgendaPage.js
+++ ClientApp/src/components/pages/agenda/AgendaPage.js
@@ -2,8 +2,7 @@ import { Container } from "@mui/system";
 
 import { Button, Typography } from "@mui/material";
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+
 import Box from '@mui/material/Box';
 import React from 'react'
 import config from "../../../config.json";
@@ -13,21 +12,21 @@ import Voorstelling2 from '../medewerker/Voorstelling2.json';
 import Voorstelling3 from '../medewerker/Voorstelling3.json';
 
 export default function AgendaPage() {
-    document.title = "Medewerker" + config.title;
+    document.title = "Agenda" + config.title;
     var voorstellingen = [Voorstelling1, Voorstelling2, Voorstelling3];
 
     return(
         <>
-            <div style={{height:"100px"}}>
-                <Container width="200" sx={{py:2}}>
+            <div style={{position: "relative", width:"90%", height:"90%"}}>
+                <Container sx={{py:2, position: "relative", width:"90%", height:"90%"}}>
                     <Typography sx={{fontSize:42, fontWeight:"medium"}}>Agenda</Typography>
                 </Container>
             </div>
-            <Container sx={{ width: "100%", display: { sm: "flex", xs: "block" } }}>
+            <Container display="flex" >
                 <div style={{
                     alignContent: "top",
                     height: "300px",
-                    
+                    position: "relative",
                     justifyContent: "center",
                     display: "block",
                     pt: 10,
@@ -41,43 +40,83 @@ export default function AgendaPage() {
     );
 }
 
+
+
 function Item(props){
     return (
+        <>
         <Container sx={{ py:2}}>
-            <Card variant="outlined"sx={{ width: 1325, height:250}}>
-                <Box sx={{ display: 'flex' }}>
-                    <CardMedia
-                        component="img"
-                        sx={{ height: 250, width: 250}}
-                        image={props.voorstelling.afbeelding}
-                        alt={props.voorstelling.titel}
-                    />
-                    <CardContent>
-                        <div style={{padding:15}}>
-                            <Typography sx={{ fontSize: 42 , fontWeight:"medium"}} color="black" gutterBottom>
-                            {props.voorstelling.titel}
-                            </Typography>
-                            <Typography sx={{ fontSize: 18, width:750}} color="black" gutterBottom>
-                            {props.voorstelling.beschrijving}
-                            </Typography>
-                        </div>
-                        
+            <Card>
+                <Box sx={{
+                    display: "flex",
+                    gap: 2,
+                    flexDirection: {
+                        sm: "column-reverse",
+                        md: "row",
+                        xs: "column",
+                    },
+                    alignItems: "flex-start",
+                }}>
+                    
+                    <Box sx={{display:"flex"}} >
+                        <img
+                            src={props.voorstelling.afbeelding}
+                            alt={props.voorstelling.titel}
+                            style={{
+                                maxWidth: "12rem",
+                                maxHeight: "12rem",
+                                height: "20vh",
+                                width: "20vh",
+                            }}
+                            
+                            
+                            />
+                        <Box>
+                            <Box sx={{ py:3, alignContent: "center", textAlign: "center", fontWeight: 'bold', m: 1, fontSize:40,height:75,width:130}}>
 
-                    </CardContent>
-                    <Card>
-                        <Box sx={{padding:3, textAlign: "center", fontWeight: 'bold', m: 1, fontSize:50,height:75, width:250}}>{props.voorstelling.datum}</Box>
-                        <Box sx={{padding:1, textAlign: "center", fontWeight: 'medium', m: 1, fontSize:20, height:50 }}>
-                            <Typography sx={{ fontSize: 20 , fontWeight:"regular"}} color="black" gutterBottom>
-                            {props.voorstelling.tijd+" "}
-                            {"zaal: " + props.voorstelling.zaal}
-                            </Typography>
+                                {props.voorstelling.datum}
+
+                            </Box>
+                            <Box sx={{ py:2, textAlign: "center", fontWeight: 'medium', m: 1, fontSize:20, height:50 }}>
+                                <Typography sx={{ fontSize: 20 , fontWeight:"regular"}} color="black" gutterBottom>
+                                {props.voorstelling.tijd+" "}
+                                {"zaal: " + props.voorstelling.zaal}
+                                </Typography>
+                            </Box>
                         </Box>
-                        <Button sx={{height:100, fontSize:24}}size="large" color="primary" fullWidth="true" variant="contained">
+
+
+                        
+                    </Box>
+                    <Box >
+                            <div style={{padding:'0.5vw' , height: "18vh"}}>
+                                <Typography sx={{ fontSize: '2vh' , fontWeight:"medium"}} color="black" gutterBottom>
+                                {props.voorstelling.titel}
+                                </Typography>
+                                <Typography sx={{ fontSize: '1.5vh'}} maxWidth="500" maxHeight="100" color="black" gutterBottom>
+                                {props.voorstelling.beschrijving}
+                                </Typography>
+                            </div>
+                        </Box>            
+
+                    
+                    <Box>
+                        
+                    </Box>
+
+
+
+                    
+                    
+                </Box>
+                
+
+
+                <Button sx={{height:50, fontSize:20}}size="large" color="primary" fullWidth="true" variant="contained">
                             Tickets
                         </Button>
-                    </Card>
-                </Box>
             </Card>
         </Container>
+        </>
     )
 }
