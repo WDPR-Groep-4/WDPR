72c6ec588dff7cb6efea1f6e6e0eb1905c31b87d
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-01-17 19:34:27 +0100

    filterSorteer drawer

--- ClientApp/src/components/pages/agenda/ZoekOpties/FilterSort/FilterSortButton.js
+++ ClientApp/src/components/pages/agenda/ZoekOpties/FilterSort/FilterSortButton.js
@@ -1,10 +1,28 @@
 import { IconButton } from "@mui/material";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import { useState } from "react";
+import { useRef, useEffect } from "react";
+import DrawerCompleet from "./DrawerCompleet";
 
 export default function FilterSortButton() {
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const anchorRef = useRef(null);
+
+    function handleClose() {
+        setOpenDrawer(false);
+    }
+
     return (
-        <IconButton sx={{ p: 2 }}>
-            <FilterListIcon />
-        </IconButton>
+        <>
+            <DrawerCompleet
+                openDrawer={openDrawer}
+                handleClose={handleClose}
+                anchorRef={anchorRef}
+                title="Filteren en sorteren"
+            ></DrawerCompleet>
+            <IconButton sx={{ p: 2 }} onClick={() => setOpenDrawer(!openDrawer)}>
+                <FilterListIcon />
+            </IconButton>
+        </>
     );
 }
