58d81d402290a44d29d8eccecc3a5d563e68f88c
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2023-01-20 17:44:12 +0100

    Update HuurEventController.cs

--- Controllers/Planning/HuurEventController.cs
+++ Controllers/Planning/HuurEventController.cs
@@ -18,11 +18,44 @@ public class HuurEventController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> HuurZaal(HuurEventDto huurEventDto)
     {
+        if (huurEventDto.AantalUren < 1 || huurEventDto.AantalUren > 8)
+        {
+            return BadRequest("Incorrect aantal uren");
+        }
         if (huurEventDto.ZaalId < 1 || huurEventDto.ZaalId > 4)
         {
             return BadRequest("Incorrect zaalid");
         }
+        if (huurEventDto.Start < DateTime.Now)
+        {
+            return BadRequest("Start is al geweest");
+        }
 
+        var verhuurEvent = new VerhuurEvent
+        {
+            Eigenaar = huurEventDto.Email,
+            Zaal = huurEventDto.ZaalId,
+            DatumBereik = new DatumBereik
+            {
+                Van = huurEventDto.Start,
+                Tot = huurEventDto.Start.AddHours(huurEventDto.AantalUren)
+            }
+        };
+        
+        DatumBereik datumBereik = new DatumBereik
+        {
+            Van = huurEventDto.Start,
+            Tot = huurEventDto.Start.AddHours(huurEventDto.AantalUren)
+        };
+
+        if (!PlanningUtils.IsDatumVrij(verhuurEvent.DatumBereik, verhuurEvent.Zaal, _context))
+        {
+            return BadRequest("Datum en/of zaal is niet beschikbaar");
+        }
+
+        await _context.Events.AddAsync(verhuurEvent);
+        await _context.SaveChangesAsync();
+        return Ok();
     }
 
 }
@@ -30,6 +63,7 @@ public class HuurEventController : ControllerBase
 public class HuurEventDto
 {
     public string Email { get; set; }
-    public DatumBereik DatumBereik { get; set; }
+    public DateTime Start { get; set; }
+    public int AantalUren { get; set; }
     public int ZaalId { get; set; }
 }
