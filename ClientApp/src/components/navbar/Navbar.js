2eaa78667034979e0e33d939f59ddb92e1c163a9
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2023-01-21 16:03:56 +0100

    Nav, footer, home fixes

--- ClientApp/src/components/navbar/Navbar.js
+++ ClientApp/src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import {
-    AppBar,
-    Box,
-    Button,
-    IconButton,
-    ListItem,
-    Toolbar,
-    Typography,
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  ListItem,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import { Stack } from "@mui/system";
 import { Link } from "react-router-dom";
@@ -16,60 +17,60 @@ import MobileMenuButton from "./MobileMenuButton";
 import account from "./account.json";
 
 export default function Navbar(props) {
-    const navLinks = [
-        { name: "Home", path: "/" },
-        { name: "Agenda", path: "/agenda" },
-        { name: "Steun ons", path: "/steun-ons" },
-    ];
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "Agenda", path: "/agenda" },
+    { name: "Steun ons", path: "/steun-ons" },
+  ];
 
-    return (
-        <AppBar position="static">
-            <Toolbar sx={{ width: "100%", maxWidth: 1536, mx: "auto" }}>
-                <Typography
-                    variant="h5"
-                    component="div"
-                    sx={{ fontWeight: "bold", flexGrow: 1, fontSize: 24 }}
-                >
-                    <Link to="/">Theater Laak</Link>
-                </Typography>
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ width: "100%", maxWidth: 1536, mx: "auto" }}>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ fontWeight: "bold", flexGrow: 1, fontSize: 24 }}
+        >
+          <Link to="/">Theater Laak</Link>
+        </Typography>
 
-                <Stack
-                    direction={"row"}
-                    spacing={1.5}
-                    sx={{ display: { xs: "none", sm: "block" } }}
-                >
-                    {navLinks.map((link) => (
-                        <Box
-                            key={link.name}
-                            component={Link}
-                            to={link.path}
-                            sx={{ width: "max-content" }}
-                        >
-                            <Button
-                                color="inherit"
-                                sx={{
-                                    fontWeight: 600,
-                                }}
-                                variant="text"
-                            >
-                                {link.name}
-                            </Button>
-                        </Box>
-                    ))}
-                </Stack>
+        <Stack
+          direction={"row"}
+          spacing={1.5}
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
+          {navLinks.map((link) => (
+            <Button
+              tabIndex={0}
+              key={link.name}
+              component={Link}
+              to={link.path}
+              color="inherit"
+              sx={{
+                fontWeight: 600,
+              }}
+              variant="text"
+            >
+              {link.name}
+            </Button>
+          ))}
+        </Stack>
 
-                <Stack direction={"row"}>
-                    <AccountButton account={account}>
-                        <PersonIcon />
-                    </AccountButton>
-                    <ListItem component={Link} to="/winkelwagen">
-                        <IconButton color="inherit" sx={{ fontWeight: 600 }}>
-                            <ShoppingCartIcon />
-                        </IconButton>
-                    </ListItem>
-                    <MobileMenuButton navLinks={navLinks} />
-                </Stack>
-            </Toolbar>
-        </AppBar>
-    );
+        <Stack direction={"row"}>
+          <AccountButton account={account}>
+            <PersonIcon />
+          </AccountButton>
+          <IconButton
+            color="inherit"
+            sx={{ fontWeight: 600 }}
+            component={Link}
+            to="/winkelwagen"
+          >
+            <ShoppingCartIcon />
+          </IconButton>
+          <MobileMenuButton navLinks={navLinks} />
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 }
